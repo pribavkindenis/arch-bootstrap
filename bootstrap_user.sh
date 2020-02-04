b3f2a6058b2fd743144adf2871fae590002b41b1
Denis Pribavkin <pribavkindenis@gmail.com>
2020-02-04 12:16:42 +0400

    Version 0.1 Description: - Pikaur installation fix

--- bootstrap_user.sh
+++ bootstrap_user.sh
@@ -24,11 +24,15 @@ packages=(
 function install_pikaur()
 {
     section "Install Pikaur"
-    git clone https://aur.archlinux.org/pikaur.git
-    cd pikaur
-    makepkg -fsri
-    cd ..
-    rm -rf pikaur
+    function executable()
+    {
+	git clone https://aur.archlinux.org/pikaur.git
+        cd pikaur
+        makepkg -fsri
+        cd ..
+        rm -rf pikaur
+    }
+    exec_by_condition executable "Do you want to install Pikaur package manager? Y/n"
 }
 
 function install_packages()
@@ -46,5 +50,6 @@ function install_packages()
 greeting
 install_pikaur
 install_packages
+bash bs_git.sh
 farewell
 exit 0
